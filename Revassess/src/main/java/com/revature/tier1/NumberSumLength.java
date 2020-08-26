04c25b5814eb3345eed6dff0b2d4fa7e5f46bbf8
Nicole Lang <nicole.lang@revature.net>
2020-08-26 15:16:34 -0400

    Changes to be committed: 	modified:   .travis.yml 	modified:   Revassess/src/main/java/com/revature/tier1/CompareStrings.java 	modified:   Revassess/src/main/java/com/revature/tier1/NumberSumLength.java 	modified:   Revassess/src/main/java/com/revature/tier1/SumOverArray.java 	modified:   Revassess/src/main/java/com/revature/tier1/User.java

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -1,8 +1,28 @@
 package com.revature.tier1;
 
+import java.util.Arrays;
+
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
-		return false;
+		String strNum = num + "";
+		int numLength = strNum.length();
+		System.out.println(numLength);
+		long sum = 0;
+		long[] digits = new long[numLength];
+		for(int i = 0; i < numLength; i++)
+		{
+			digits[i] = Long.parseLong(strNum.charAt(i) + "");
+			digits[i] = (long) Math.pow(digits[i], numLength);
+
+			System.out.println(digits[i]);
+			sum += digits[i];
+		}
+		System.out.println("sum = " + sum);
+		System.out.println("next case");
+		if(sum == num)
+			return true;
+		else
+			return false;
 	}
 }
