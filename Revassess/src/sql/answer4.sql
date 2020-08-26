3efabcf2eeb0a272a85117c9ff863171f497910a
Nicole Lang <nicole.lang@revature.net>
2020-08-26 17:53:20 -0400

    fixed the properties (still couldn't figure out 4)

--- Revassess/src/sql/answer4.sql
+++ Revassess/src/sql/answer4.sql
@@ -1,2 +1,5 @@
 --i don't know--
-select * from study_set where owner_id = 4;
+select "name", username from study_set
+join app_user on (study_set.owner_id = app_user.user_id);
+select category_name, question, answer from flashcard join category 
+on (flashcard.category_id = category.category_id )
